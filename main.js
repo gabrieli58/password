c4ec30ca43f5603453f8b6aa8fae9592d6b82fe1
gabrieli58 <gabrieli.faustino.rodrigues@escola.pr.gov.br>
2025-06-05 08:19:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+document.querySelector('.parametro-senha__texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+const numero = document.querySelector('.parametro-senha__texto');
+numeroSenha.textContent = 5;
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+numeroSenha.textContent = 5;
+const botoes = document.querySelector('.');
+const botao = document.querySelector('.parametro-senha__botao');
+const senha__botao = document.querySelector('.parametro-senha__botao');
+
+console.log(botoes);
+const senha_botao = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+console.log(botoes);
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+console.log(botoes);
+
+function diminuiTamanho(){ 
+
+}
+let tamanhoSenha = 12;
+senha.textContent = tamanhoSenha;
+tamanhoSenha = tamanhoSenha - 1;
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanho = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+
+function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
+}
+
+console.log(botoes);
+function diminuiTamanho(){ 
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha
+}
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+function aumentaTamanho() {
+    tamanhoSenha = tamanhoSenha + 1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+function diminuiTamanho() {
+    if (tamanhoSenha > 1)
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+            tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
+        // tamanhoSenha = tamanhoSenha - 1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+            // tamanhoSenha = tamanhoSenha + 1;
+            tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+const numeroSenha = document.querySelector('.parametro-senha__texto');
